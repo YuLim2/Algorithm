623e2b1526e1060b02870a946b28c38353e2f22c
YuLim2 <80025122+YuLim2@users.noreply.github.com>
2021-10-27 20:18:50 +0900

    no message

--- 순환 알고리즘/하노이탑_1.cpp
+++ 순환 알고리즘/하노이탑_1.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-void hanoi(int n, char a, char b, char c){
+void hanoi(int n, char a, char b, char c){	//내부의 자리가 바뀌어도 지정한 abc는 고 
 	if(n<=1){
 		printf("%d:%c->%c\n", n, a, c);
 	} else{
@@ -12,8 +12,8 @@ void hanoi(int n, char a, char b, char c){
 }
 
 int main(){
-	int n=4;
-	scanf("%d", &n);
+	int n=4;	
+	scanf("%d", &n);정 
 	hanoi(n, 'A', 'B', 'C');
 	printf("%d %d", cnt, cnt1);
 	return 0;
